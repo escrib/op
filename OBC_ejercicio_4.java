98c4d09196e6e0861ef2818ceecc8f3590f90024
escrib <sebascraton@gmail.com>
2022-06-25 22:12:25 -0300

    Ejercicio_4

---
+++ OBC_ejercicio_4.java
@@ -0,0 +1,15 @@
+
+public class If {
+    public static void main(String args[]) {
+      
+        int numeroif;
+        if (numeroif > 0) {
+            System.out.println("positivo");
+        } else if (numeroif == 0) {
+            System.out.println("cero");
+        } else {
+            System.out.println("negativo");
+        }
+      }
+    }
+  
